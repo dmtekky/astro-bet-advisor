41783a18db98852aecb8010d21f1d1d5fab21dba
DMTekk <ddberryjr@gmail.com>
2025-05-20 15:43:50 -0400

    Fix MIME type and routing issues for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/' : '/',
+  base: '/',
   plugins: [
     react({
       // Only import React when needed
