27107498ce2c9567239b8efd0d197785baff0640
DMTekk <ddberryjr@gmail.com>
2025-05-20 15:39:19 -0400

    Fix MIME type error and Dialog accessibility warning

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => ({
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   plugins: [
     react({
       // Only import React when needed
